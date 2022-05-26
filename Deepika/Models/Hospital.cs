02489965a083b4b177a57d7ef000467cd233a369
unknown <deepikas.devnet@gmail.com>
2022-05-26 00:09:20 -0500

    test

---
+++ Deepika/Models/Hospital.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Deepika.Models
+{
+    public class Hospital
+    {
+
+        [Required(ErrorMessage = "Please enter Hospital name")]
+        public string Name { get; set; }
+        
+        [Required(ErrorMessage = "Please enter the Hospital Address")]
+        public string Address { get; set; }
+       
+        [Required(ErrorMessage = "Please select the speciality")]
+        public string Speciality { get; set; }
+
+    }
+}
---
+++ Deepika/Models/Repository.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+
+namespace Deepika.Models
+{
+
+    public static class Repository
+    {
+        private static List<Hospital> allHospitals = new List<Hospital>();
+        public static IEnumerable<Hospital> AllHospitals
+        {
+            get { return allHospitals; }
+        }
+        public static void Create(Hospital hospital)
+        {
+            allHospitals.Add(hospital);
+        }
+
+        public static void Delete(Hospital hospital)
+        {
+            allHospitals.Remove(hospital);
+        }
+    }
+}
